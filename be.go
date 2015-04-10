e9ccec44e180b0d39502dad7c452fe25d55d783f
Ariel Mashraki <ariel@mashraki.co.il>
2015-04-10 23:49:32 +0300

    feat(expect/be): Above/Below

--- be.go
+++ be.go
@@ -1,18 +1,42 @@
-package should
+package expect
 
 import (
-	"fmt"
+	. "fmt"
 	"testing"
 )
 
 type Be struct {
 	*testing.T
-	Actual interface{}
+	actual interface{}
+	assert bool
 }
 
-func (b *Be) Above(e interface{}) {
-	if i, ok := e.(int); ok {
-
+func (b *Be) Above(e int) {
+	msg := b.msg(Sprintf("above %v", e))
+	if b.Int() > e != b.assert {
+		b.Error(msg)
 	}
-	b.Error("foo")
+}
+
+func (b *Be) Below(e int) {
+	msg := b.msg(Sprintf("below %v", e))
+	if b.Int() < e != b.assert {
+		b.Error(msg)
+	}
+}
+
+func (b *Be) msg(s string) string {
+	not := "not "
+	if b.assert {
+		not = ""
+	}
+	return Sprintf("Expect %v %vto be %v", b.actual, not, s)
+}
+
+func (b *Be) Int() int {
+	if i, ok := b.actual.(int); ok {
+		return i
+	}
+	b.Fatal("Invalid argument, expect to int")
+	return 0
 }
--- expect.go
+++ expect.go
@@ -1,13 +1,27 @@
-package should
+package expect
 
 import (
-	"fmt"
 	"testing"
 )
 
-func New(t *testing.T) func(v interface{}) {
-	return func(v interface{}) {
-		fmt.Println(v, t)
-		t.Error("fail")
+type Expect struct {
+	To  *to
+	Not *not
+}
+
+type to struct {
+	Be *Be
+}
+
+type not struct {
+	To *to
+}
+
+func New(t *testing.T) func(v interface{}) *Expect {
+	return func(v interface{}) *Expect {
+		return &Expect{
+			To:  &to{&Be{t, v, true}},
+			Not: &not{&to{&Be{t, v, false}}},
+		}
 	}
 }
